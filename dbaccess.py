5fde5c7e473bc7079b2b9e0a060a6d0cfd70a351
demisequartet <kaizukasi551@gmail.com>
2021-11-10 18:38:15 +0900

    postgres dockerfile add

--- dbaccess.py
+++ dbaccess.py
@@ -2,12 +2,21 @@ import psycopg2
 
 print(psycopg2.apilevel)
 
-DATABASE_URL = "postgresql://postgre:postgre@localhost:5432/postgres"
+DATABASE_URL = "postgresql://postgres:Password@localhost:5432/db"
+
+
+def getCon():
+    return psycopg2.connect(DATABASE_URL)
 
 
 def main():
-    cursor = psycopg2.connect(DATABASE_URL)
-    print(cursor)
+    conn = getCon()
+    cur = conn.cursor()
+    cur.execute('SELECT tablename from pg_tables')
+    for row in cur:
+        print(row)
+    cur.close()
+    conn.close()
 
 
 if __name__ == "__main__":
