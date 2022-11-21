75892f79cec3b1f38b7732f1d26d16aa75b05b80
demisequartet <kaizukasi551@gmail.com>
2022-11-21 14:03:20 +0900

    理解度テスト第1回用の問題文改善、全角スペースの検出機能追加

--- db/all.sql
+++ db/all.sql
@@ -24,16 +24,11 @@ CREATE TABLE submit (
 
 );
 
+
 INSERT INTO question (question_id, name, question_content,output, difficulty, basename, commentary) VALUES
-(1, 'HELLO WORLD', 'Hello,World!を出力するプログラムを作成せよ。末尾の改行の有無は問わない',E'Hello,World!\n','NONE', '', '');
-INSERT INTO question (question_id, name, question_content,output, difficulty, basename, commentary) VALUES
-(2, 'wakayama repeat', '出力例のように、wakayamaを10回表示するプログラムを作成せよ。',E'wakayama\nwakayama\nwakayama\nwakayama\nwakayama\nwakayama\nwakayama\nwakayama\nwakayama\nwakayama\n','NONE', '', '');
-INSERT INTO question (question_id, name, question_content,output, difficulty, basename, commentary) VALUES
-(3, 'okayama repeat', 'okayamaを10回表示するプログラムを作成せよ。ただし、okayamaと1回表示したあとに改行すること',E'okayama\nokayama\nokayama\nokayama\nokayama\nokayama\nokayama\nokayama\nokayama\nokayama\n','NONE', '', '');
-INSERT INTO question (question_id, name, question_content,output, difficulty, basename, commentary) VALUES
-(4,'prime number','2から30までの素数を出力せよ。',E'2\n3\n5\n7\n11\n13\n17\n19\n23\n29\n','NONE','','');
-INSERT INTO question (question_id, name, question_content,output, difficulty, basename, commentary) VALUES
-(5,'HELLO PROGRAMMING','出力例のように、Hello C Programmingと表示するプログラムを作成せよ。なお、printf関数を何回使っても良い。',E'Hello\nC\nProgramming\n','NONE','','');
+(1,'HELLO PROGRAMMING','出力例のように、「Hello」 「C」 「Programming」に分けて3行で表示するプログラムを作成してください。なお、printf関数は何回使っても構いません。',E'Hello\nC\nProgramming\n','NONE','','');
+-- INSERT INTO question (question_id, name, question_content,output, difficulty, basename, commentary) VALUES
+-- (2,'九九','2から30までの素数を出力せよ。',E'2\n3\n5\n7\n11\n13\n17\n19\n23\n29\n','NONE','','');
 
 
 
